aca98d19c4d0296c434f123fa01a94abc920f710
ruan-cardozo <ruanvcardozo@gmail.com>
2025-03-23 14:52:02 -0300

    feat: implementing count lines

--- cmd/count_lines_cmd.go
+++ cmd/count_lines_cmd.go
@@ -5,35 +5,69 @@ import (
 	"go-cli-tool/internal/analyzer"
 	"go-cli-tool/internal/policies"
 	"go-cli-tool/internal/utils"
+	"go-cli-tool/templates"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-var filePath string
-
 var countLinesCmd = &cobra.Command{
     Use:   "count-lines",
     Short: "Count total lines and comment lines in a JavaScript file",
     Run: func(cmd *cobra.Command, args []string) {
-        if !policies.ValidateFilePath(filePath) {
-            fmt.Printf("%sPlease provide the path to the JavaScript file using the -f flag or use.%s", utils.Red, utils.ResetColor)
-            os.Exit(1)
+
+        validateUserInput()
+
+        if filePath != "" {
+            result := analyzer.CountLinesByFilePath(filePath)
+            fmt.Printf("%sTotal lines:%s %d\n", utils.BLUE, utils.RESET_COLOR, result.TotalLines)
         }
 
-        if !policies.IsJSFileExtension(filePath) {
-            fmt.Printf("%sOnly JavaScript files are accepted.%s", utils.Red, utils.ResetColor)
-            os.Exit(1)
+        result, totalLinesByDirectory := analyzer.CountLinesByDirectory(directoryPath)
+
+        if outputFilePath != "" {
+            templates.SaveResultsToHTML(result, totalLinesByDirectory, outputFilePath, utils.COUNT_LINES)
+        } else {
+            printResults(result, totalLinesByDirectory)
         }
-
-        result := analyzer.CountLines(filePath)
-
-        fmt.Printf("%sTotal lines:%s %d\n", utils.Blue, utils.ResetColor, result.TotalLines)
     },
 }
 
+func printResults(result analyzer.FilesNameCountLineMap, totalLinesByDirectory analyzer.LineResult) {
+    for fileName, result := range result {
+
+        fmt.Printf("%s Total lines in %s:%s %d\n", utils.BLUE, fileName, utils.RESET_COLOR, result.TotalLines)
+    }
+    fmt.Printf("%sTotal lines in directory:%s %d\n", utils.BLUE, utils.RESET_COLOR, totalLinesByDirectory.TotalLines)
+}
+
+func validateUserInput() {
+
+    if filePath == "" && directoryPath == "" {
+        fmt.Printf("Please provide the path to the JavaScript file using the -f flag or use the -d flag to provide the path to the directory containing the JavaScript files.\n")
+        os.Exit(1)
+    }
+
+    if filePath != "" {
+        validateFilePath()
+    }
+}
+
+func validateFilePath() {
+    if !policies.ValidateFilePath(filePath) {
+        fmt.Printf("%sPlease provide the path to the JavaScript file using the -f flag or use.%s", utils.RED, utils.RESET_COLOR)
+        os.Exit(1)
+    }
+
+    if !policies.IsJSFileExtension(filePath) {
+        fmt.Printf("%sOnly JavaScript files are accepted.%s", utils.RED, utils.RESET_COLOR)
+        os.Exit(1)
+    }
+}
+
 func init() {
-    countLinesCmd.Flags().StringVarP(&filePath, "file", "f", "", "Path to the JavaScript file or directory")
-    countLinesCmd.MarkFlagRequired("file")
+    countLinesCmd.Flags().StringVarP(&filePath, "file", "f", "", "Path to the JavaScript file (must be a single file, not a directory)")
+    countLinesCmd.Flags().StringVarP(&directoryPath, "directory", "d", "", "Path to the directory containing JavaScript files. The tool will automatically expand the provided path.")
+    countLinesCmd.Flags().StringVarP(&outputFilePath, "output", "o", "", "Path to the output file. The tool will generate an HTML report with the results. If not provided, the tool will print the results to the console.")
     rootCmd.AddCommand(countLinesCmd)
 }
--- internal/analyzer/count_lines.go
+++ internal/analyzer/count_lines.go
@@ -2,10 +2,20 @@ package analyzer
 
 import (
 	"bufio"
-	"os"
-)
 
-func CountLines(filePath string) LineResult {
+	// "fmt"
+	"go-cli-tool/internal/policies"
+	"io/fs"
+	"os"
+	"os/user"
+	"path/filepath"
+	"slices"
+)
+type FilesNameCountLineMap map[string]LineResult
+var directoryOrFilesToIgnore = []string{".git", "node_modules"}
+var totalLinesByDirectory LineResult
+
+func CountLinesByFilePath(filePath string) LineResult {
     file, err := os.Open(filePath)
 
     if err != nil {
@@ -31,6 +41,74 @@ func CountLines(filePath string) LineResult {
     return result
 }
 
+func CountLinesByDirectory(directoryPath string) (FilesNameCountLineMap, LineResult) {
+
+    if directoryPath == "." {
+        var err error
+        directoryPath, err = os.Getwd()
+        if err != nil {
+            panic(err)
+        }
+    } else {
+        var err error
+        directoryPath, err = expandPath(directoryPath)
+        if err != nil {
+            panic(err)
+        }
+    }
+
+    linesByArchive := make(FilesNameCountLineMap)
+
+    err := filepath.WalkDir(directoryPath, func(path string, directory fs.DirEntry, err error) error {
+
+        if err != nil {
+            return err
+        }
+
+        fileOrDirectoryName := directory.Name()
+        fileExtension := filepath.Ext(fileOrDirectoryName)
+
+        if slices.Contains(directoryOrFilesToIgnore, fileOrDirectoryName) {
+
+            if directory.IsDir() {
+                return filepath.SkipDir
+            }
+
+            return nil
+        }
+
+        if policies.IsJSFileExtension(fileExtension) {
+
+            linesByArchive[fileOrDirectoryName] = CountLinesByFilePath(path)
+        }
+
+        return nil
+    })
+
+    if err != nil {
+        panic(err)
+    }
+
+    for result := range linesByArchive {
+
+        file := linesByArchive[result]
+        totalLinesByDirectory.TotalLines += file.TotalLines
+    }
+
+    return linesByArchive, totalLinesByDirectory
+}
+
 func isEmptyLine(line string) bool {
     return len(line) == 0
+}
+
+func expandPath(path string) (string, error) {
+    if path[:2] == "~/" {
+        usr, err := user.Current()
+        if err != nil {
+            return "", err
+        }
+        path = filepath.Join(usr.HomeDir, path[2:])
+    }
+    return path, nil
 }
