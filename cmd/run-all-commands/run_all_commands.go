de65b53246e79473fe38f5df4ea5489ddb1d3d4d
ruan-cardozo <ruanvcardozo@gmail.com>
2025-05-04 16:18:42 -0300

    feat: adding json output for analyze command

--- cmd/run-all-commands/run_all_commands.go
+++ cmd/run-all-commands/run_all_commands.go
@@ -1,9 +1,11 @@
 package run_all_commands
 
 import (
+	"encoding/json"
 	"fmt"
 	"go-cli-tool/internal/analyzer"
 	"go-cli-tool/internal/utils"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -57,24 +59,38 @@ of your JavaScript codebase. Use flags to customize the analysis and output form
             classFuncResults[utils.FilePath] = classAndFunctionResult
 
             if utils.OutputFilePath == "" {
+                
                 printFileResults(cmd, utils.FilePath, lineCount.TotalLines, commentCount.CommentLines, 
                     classAndFunctionResult.Classes, classAndFunctionResult.Functions)
                 return
             } else {
-                fmt.Fprintf(cmd.OutOrStdout(), "%sSaida estilo json em desenvolvimento...%s\n", utils.BLUE, utils.RESET_COLOR)
+                generateJSONOutput(cmd, utils.FilePath, lineCount.TotalLines, commentCount.CommentLines, 
+                    classAndFunctionResult.Classes, classAndFunctionResult.Functions)
+                return    
             }
         } else {
 
-            _, totalLines = lineAnalyzer.CountLinesByDirectory(utils.DirectoryPath)
-            _, totalComments = commentAnalyzer.CountCommentsByDirectory(utils.DirectoryPath)
-            _, totalClassesAndFunctions = classFuncAnalyzer.CountClassesAndFunctionsByDirectory(utils.DirectoryPath)
+            lineResults, totalLines = lineAnalyzer.CountLinesByDirectory(utils.DirectoryPath)
+            commentResults, totalComments = commentAnalyzer.CountCommentsByDirectory(utils.DirectoryPath)
+            classFuncResults, totalClassesAndFunctions = classFuncAnalyzer.CountClassesAndFunctionsByDirectory(utils.DirectoryPath)
             
+            if utils.SummaryOnly {
+                printDirectoryResults(cmd, totalLines.TotalLines, totalComments.CommentLines,
+                    totalClassesAndFunctions.Classes, totalClassesAndFunctions.Functions)
+                return
+            }
+
+            if utils.Detailed {
+                generateDetailedJSONOutput(cmd, utils.DirectoryPath, lineResults, commentResults, classFuncResults)
+                return
+            }
+
             if utils.OutputFilePath == "" {
                 printDirectoryResults(cmd, totalLines.TotalLines, totalComments.TotalComments, 
                     totalClassesAndFunctions.Classes, totalClassesAndFunctions.Functions)
                 return
             } else {
-                fmt.Fprintf(cmd.OutOrStdout(), "%sSaida estilo json em desenvolvimento...%s\n", utils.BLUE, utils.RESET_COLOR)
+                generateJSONOutput(cmd, utils.DirectoryPath, totalLines.TotalLines, totalComments.CommentLines, totalClassesAndFunctions.Classes, totalClassesAndFunctions.Functions)
             }
         }
     },
@@ -98,8 +114,79 @@ func printDirectoryResults(cmd *cobra.Command, lines, comments, classes, functio
     fmt.Fprintf(cmd.OutOrStdout(), "Functions: %s%d%s\n", utils.GREEN, functions, utils.RESET_COLOR)
 }
 
+func generateJSONOutput(cmd *cobra.Command, filePath string, lineCount int, commentCount int, classes int, functions int) {
+    result := map[string]interface{}{
+        "file_path": filePath,
+        "lines":     lineCount,
+        "comments":  commentCount,
+        "classes":   classes,
+        "functions": functions,
+    }
+
+    if utils.OutputFilePath != "" {
+        outputPath := utils.OutputFilePath
+
+        fileInfo, err := os.Stat(outputPath)
+        if err == nil && fileInfo.IsDir() {
+            outputPath = fmt.Sprintf("%s/analysis_report.json", outputPath)
+        }
+
+        file, err := os.Create(outputPath)
+        if err != nil {
+            fmt.Fprintf(cmd.OutOrStderr(), "Error creating JSON file: %s\n", err)
+            return
+        }
+        defer file.Close()
+
+        encoder := json.NewEncoder(file)
+        encoder.SetIndent("", "  ") // Format JSON with indentation
+        if err := encoder.Encode(result); err != nil {
+            fmt.Fprintf(cmd.OutOrStderr(), "Error writing JSON to file: %s\n", err)
+            return
+        }
+
+        fmt.Fprintf(cmd.OutOrStdout(), "JSON report saved to %s\n", outputPath)
+    } else {
+        formattedJSON, err := json.MarshalIndent(result, "", "  ")
+        if err != nil {
+            fmt.Fprintf(cmd.OutOrStderr(), "Error formatting JSON: %s\n", err)
+            return
+        }
+
+        fmt.Fprintf(cmd.OutOrStdout(), "%s\n", string(formattedJSON))
+    }
+}
+
+func generateDetailedJSONOutput(cmd *cobra.Command, directoryPath string, lineResults analyzer.FilesNameCountLineMap, commentResults analyzer.CommentsMap, classFuncResults analyzer.ClassesAndFunctionsMap) {
+    detailedResult := map[string]interface{}{
+        "directory_path": directoryPath,
+        "files":          []map[string]interface{}{},
+    }
+
+    for filePath, lineResult := range lineResults {
+        fileDetails := map[string]interface{}{
+            "file_path": filePath,
+            "lines":     lineResult.TotalLines,
+            "comments":  commentResults[filePath].CommentLines,
+            "classes":   classFuncResults[filePath].Classes,
+            "functions": classFuncResults[filePath].Functions,
+        }
+        detailedResult["files"] = append(detailedResult["files"].([]map[string]interface{}), fileDetails)
+    }
+
+    formattedJSON, err := json.MarshalIndent(detailedResult, "", "  ")
+    if err != nil {
+        fmt.Fprintf(cmd.OutOrStderr(), "Error formatting detailed JSON: %s\n", err)
+        return
+    }
+
+    fmt.Fprintf(cmd.OutOrStdout(), "%s\n", string(formattedJSON))
+}
+
 func init() {
     RunAllCommand.Flags().StringVarP(&utils.FilePath, "file", "f", "", "Path to the JavaScript file (must be a single file, not a directory)")
     RunAllCommand.Flags().StringVarP(&utils.DirectoryPath, "directory", "d", "", "Path to the directory containing JavaScript files. The tool will automatically expand the provided path.")
-    RunAllCommand.Flags().StringVarP(&utils.OutputFilePath, "output", "o", "", "Specify the output file path. If omitted, results will be displayed in the terminal. For directory analysis, only a summary will be shown if no output file is provided.")
+    RunAllCommand.Flags().StringVarP(&utils.OutputFilePath, "output", "o", "", "Specify the output file path. If omitted, results will be displayed in the terminal.")
+    RunAllCommand.Flags().BoolVar(&utils.SummaryOnly, "summary", false, "Show only a summary of the analysis.")
+    RunAllCommand.Flags().BoolVar(&utils.Detailed, "detailed", false, "Show detailed analysis, including per-file information.")
 }
--- internal/utils/common.go
+++ internal/utils/common.go
@@ -3,3 +3,5 @@ package utils
 var FilePath string
 var DirectoryPath string
 var OutputFilePath string
+var SummaryOnly bool
+var Detailed bool
